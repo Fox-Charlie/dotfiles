d4e6e9544528fed31b54ce9bb6123eca8a19717a
Charlie Fox <charlie.fox@modo.energy>
2026-01-25 22:41:45 +0000

    chore: better lsp for python

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,40 +1,47 @@
--- Mason first
 require("mason").setup()
-
 require("mason-lspconfig").setup({
     ensure_installed = { "lua_ls", "basedpyright", "ts_ls", "rust_analyzer" },
-    automatic_install = true,
 })
-
 require("mason-tool-installer").setup({
     ensure_installed = { "stylua", "prettier", "ruff", "eslint_d" },
 })
 
--- Global LSP defaults (Neovim 0.11+)
-local capabilities = require("blink.cmp").get_lsp_capabilities()
 local fzf = require("fzf-lua")
 
-local on_attach = function(client, bufnr)
-    local map = function(mode, lhs, rhs, desc)
-        vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-    end
-    map("n", "gd", vim.lsp.buf.definition, "Go to definition")
-    map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
-    map("n", "gr", fzf.lsp_references, "Go to references")
-    map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
-    map("n", "gt", vim.lsp.buf.type_definition, "Go to type definition")
-    map("n", "K", vim.lsp.buf.hover, "Hover documentation")
-    map("n", "<C-k>", vim.lsp.buf.signature_help, "Signature help")
-    map("n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol")
-    map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code action")
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local bufnr = args.buf
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        local map = function(mode, lhs, rhs, desc)
+            vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+        end
 
-    -- Enable inlay hints if supported
-    if client.server_capabilities.inlayHintProvider then
-        vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-    end
-end
+        map("n", "gd", vim.lsp.buf.definition, "Go to definition")
+        map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+        map("n", "gr", fzf.lsp_references, "Go to references")
+        map("n", "gi", vim.lsp.buf.implementation, "Go to implementation")
+        map("n", "gt", vim.lsp.buf.type_definition, "Go to type definition")
+        map("n", "K", vim.lsp.buf.hover, "Hover documentation")
+        map("n", "<C-k>", vim.lsp.buf.signature_help, "Signature help")
+        map("n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+        map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code action")
 
-vim.lsp.config('*', { capabilities = capabilities, on_attach = on_attach })
-vim.lsp.config('basedpyright', {
-    settings = { basedpyright = { analysis = { diagnosticMode = "workspace" } } },
+        if client and client.server_capabilities.inlayHintProvider then
+            vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+        end
+    end,
 })
+
+vim.lsp.config("*", {
+    capabilities = require("blink.cmp").get_lsp_capabilities(),
+})
+
+vim.lsp.config("basedpyright", {
+    before_init = function(_, config)
+        if vim.env.VIRTUAL_ENV then
+            config.settings.python = { pythonPath = vim.env.VIRTUAL_ENV .. "/bin/python" }
+        end
+    end,
+})
+
+vim.lsp.enable({ "basedpyright", "lua_ls", "ts_ls", "rust_analyzer" })
